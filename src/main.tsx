9061d319e4f17a5551ad5247ae4a3395cd0a91b8
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2023-03-07 11:14:34 -0500

    Chore: add vite-tsconfig-paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './styles/global.prod.css'
+import '@styles/global.prod.css'
 
-import { AppRoutes } from './routes/Routes'
+import { AppRoutes } from '@routes/Routes'
 
 ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
